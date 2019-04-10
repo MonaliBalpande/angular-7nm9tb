4c626c6d65222461c9c04a9b6d4eed9876a03399
MonaliBalpande <dudhabale.rupali@praxislogica.com>
2019-04-10 11:29:19 +0530

    2 files modified, 3 added

--- src/app/herose/herose.component.ts
+++ src/app/herose/herose.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
+import { HEROES } from '../heroes';
 @Component({
   selector: 'app-herose',
   templateUrl: './herose.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroseComponent implements OnInit {
 
+ hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+   onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+   }
 }
